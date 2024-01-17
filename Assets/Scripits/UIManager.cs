dc219747dd298944650f0e7bfb347603db116d20
Konstantin <inbloomf3@gmail.com>
2024-01-17 15:51:33 -0800

    fdgdf
    dfgdf

--- Assets/Scripits/UIManager.cs
+++ Assets/Scripits/UIManager.cs
@@ -15,6 +15,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private GameObject losePanel;
     [SerializeField] private GameObject inGamePanel;
+    public UniWebView uniWebView;
     private void Awake()
     {
         instance = this;
@@ -90,6 +91,11 @@ public class UIManager : MonoBehaviour
     }
     public void ShowPrivacy(string url)
     {
-
+        var webviewObject = new GameObject("UniWebview");
+        uniWebView = webviewObject.AddComponent<UniWebView>();
+        uniWebView.Frame = new Rect(0, 0, Screen.width, Screen.height);
+        uniWebView.SetShowToolbar(true, false, true, true);
+        uniWebView.Load(url);
+        uniWebView.Show();
     }
 }
